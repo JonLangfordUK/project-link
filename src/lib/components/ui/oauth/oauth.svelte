e52a9f60009524d3b7f1cbbd0f753129bd42a7ec
Jon Langford <jonlangforduk@gmail.com>
2024-09-23 12:32:17 +0100

    Fixing issue with the form action not correctly being parsed to component

--- src/lib/components/ui/oauth/oauth.svelte
+++ src/lib/components/ui/oauth/oauth.svelte
@@ -13,13 +13,13 @@
   }
 
   export let oauthData: OAuthData[] = [];
-  export let onEmailClicked: ButtonEventHandler<MouseEvent> | null | undefined;
+  export let onEmailClicked: ((e: ButtonEventHandler<MouseEvent>) => void) | null | undefined;
 
 </script>
 
 <form method="POST" use:enhance>
   {#each oauthData as { logoComponent, name, action }}
-    <Form.Button class="w-full h-12 mb-4 formaction={action}">
+    <Form.Button class="w-full h-12 mb-4" formaction={action}>
       <div class="grid grid-cols-3 gap-4 w-2/3">
         <img class="size-6 mr-4 w-full" src={logoComponent} alt={name} />
         <span class="col-span-2 w-full">{name}</span>
